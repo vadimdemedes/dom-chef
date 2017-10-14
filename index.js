e6e68475503ba63a1f8f5c02b2d2e92bafe3ebd8
Federico Brigante <github@bfred.it>
2017-10-14 12:59:00 -0500

    Fix

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const build = (tagName, attrs, children) => {
 
 	const className = attrs.class || attrs.className;
 	if (className) {
-		setAttribute(tagName, el, 'class', classnames(className));
+		setAttribute(el, 'class', classnames(className));
 	}
 
 	getCSSProps(attrs).forEach(prop => {
